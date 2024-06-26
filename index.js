40f2c14017cfa7d348c60ba65f030fa653452834
PiyushMhetre <piyushmhetre9596@gmail.com>
2024-06-26 20:21:44 +0530

    api comit

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ import { dbconnect } from './config/database.js';
 import dotenv from 'dotenv'
 dotenv.config();
 import cors from 'cors'
-
+import path from 'path'
 const app = express();
 dbconnect();
 
-//middleware to parse json request body 
+// const __dirname = path.resolve();
+// app.use(express.static(path.join(__dirname, '../frontend/build')));
+// middleware to parse json request body 
 app.use(express.json());
 
 import todoRouter from './routes/todos.js';
@@ -20,10 +22,14 @@ app.use(cors({
 }));
 app.use("/api/v1", todoRouter); 
 
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
+
 
 
 app.listen(4000, () => {
-    console.log("server started successfully")
+    console.log("server started successfully")  
 });
 
  
