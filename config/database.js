801c83bb8a19671efd055944dbda2858104481d9
PiyushMhetre <piyushmhetre9596@gmail.com>
2024-06-27 13:09:45 +0530

    Your commit message here

--- config/database.js
+++ config/database.js
@@ -1,16 +1,15 @@
-import  mongoose  from "mongoose";
-import dotenv from 'dotenv'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
 dotenv.config();
 
-export function dbconnect(){
-   
-    mongoose.connect(process.env.DATABASE_URL)
+export function dbconnect() {
+  mongoose.connect(process.env.DATABASE_URL || 'mongodb+srv://piyushmhetre9596:aXcwASYL1eQfppAc@cluster0.chtw4ix.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
-        console.log("MongoDB connected successfully");
+      console.log("MongoDB connected successfully");
     })
     .catch((error) => {
-        console.error("Error connecting to MongoDB:", error);
-        process.exit(1);
+      console.error("Error connecting to MongoDB:", error.message);
+      process.exit(1);
     });
-
 }
